c523452b25ab6ebebbaf54a953c0dea86a54249a
schlegel.felix@gmail.com <schlegel.felix@gmail.com>
2018-10-17 16:33:19 +0200

    completed bolus tracking module, small fixes

--- fMRI_notebooks/MRIhelperfunctions.py
+++ fMRI_notebooks/MRIhelperfunctions.py
@@ -1,9 +1,11 @@
 import subprocess
 import os
+import glob
 import numpy as np
 import pandas as pd
 import nibabel as nib
 import papermill as pm
+import shutil
 
 from os.path import expanduser
 home = expanduser("~")
@@ -18,8 +20,9 @@ class Settings:
 
 
 
-def runAFNI(inputstring):
-    print(inputstring)
+def runAFNI(inputstring, printout=True):
+    if printout:
+        print(inputstring)
     subprocess.run(inputstring,shell=True, check=True)
     
     
@@ -143,7 +146,7 @@ def save_nifti(data, dimensions, folders, animal, prefix):
             prefix will be combined with animal ID to filename
     Output: .nii file in output folder. filename/location is returned
     """
-    data_reshaped = data.reshape(dimensions.x,dimensions.y,dimensions.z)
+    data_reshaped = data.reshape(dimensions.x, dimensions.y, dimensions.z, -1)
     data_img = nib.Nifti1Image(data_reshaped, dimensions.affine)
     out_file = os.path.join(folders['out'],f'{prefix}_{animal}.nii')
     nib.save(data_img, out_file)
@@ -152,8 +155,119 @@ def save_nifti(data, dimensions, folders, animal, prefix):
 
 def run_module(folders, moduleName, modulInputs):
     print(f'running {moduleName} analysis...')
+    out_notebook = os.path.join(folders['animal'],f'{moduleName}_output.ipynb')
+    if os.path.exists(out_notebook):
+        print(f'animal already analyzed (delete {out_notebook} to re-run analysis)')
+        return 
     pm.execute_notebook(
        os.path.join(folders['notebooks'],f'{moduleName}.ipynb'),
        os.path.join(folders['animal'],f'{moduleName}_output.ipynb'),
        parameters = modulInputs
     )
+    
+
+def folder_check_create(base, new):
+    """
+    Checks if a folder with name 'new' (string) already exists within folder 'base' (full path)
+    if not, create it.
+    returns the newly created folder (full path)
+    """
+    newfullfolder = os.path.join(base,new)
+    if not os.path.exists(newfullfolder):
+        os.makedirs(newfullfolder)
+    return newfullfolder
+
+def copy_notebook_outputs(folders, animal, modality, info):
+    """
+    creates a new folder with name of modality inside groupstats folder (if needed)
+    opens notebook of animal which is expected to be in the animals data folder and called 'modality'_output
+    for each entry in the notebooks papermill data, will copy the corresponding file and make an entry of its location in info dataframe 
+    """
+    group_modality_folder = folder_check_create(folders['group'], modality)
+    folders[f"group_{modality}"] = group_modality_folder
+
+    notebook = pm.read_notebook(os.path.join(folders['animal'],f'{modality}_output.ipynb'))
+    for key, value in notebook.data.items():
+        info.loc[key, animal] = value
+        path, file = os.path.split(value)
+        output = os.path.join(group_modality_folder, file)
+        shutil.copy(value, output)
+        
+    return info, folders
+
+
+def select_for_group_average(folder, info, map_names, exclude='xxx343434343', row='genotype'):
+    """
+    Looks at all nii files in folder,
+    first filters by prefix string e.g. CBV_map and string to exclude (e.g. male)
+    then groups the results based on a row in the info dataframe (e.g. genotype)
+    output are two lists group1, group2 with the full path of all files in a group.
+    
+    Limitations: Must have exactly 2 groups, must be encoded with 1 or 2.
+    """
+    allfiles = os.path.join(folder,'*.nii')
+
+    files = [fn for fn in glob.glob(allfiles)
+            if exclude not in os.path.basename(fn) and
+            os.path.basename(fn).startswith(map_names)]
+
+    x = info.loc[row,:].to_dict()
+
+    x_1 = {k : v for k,v in x.items() if v == 1.0}
+    x_2 = {k : v for k,v in x.items() if v == 2.0}
+
+    group1 = []
+    group2 = []
+    for file in files:
+        for filtered in x_1.keys():
+            if filtered in file:
+                group1.append(file)
+        for filtered in x_2.keys():
+            if filtered in file:
+                group2.append(file)
+    return group1, group2
+
+
+def average_map(map_name, out_folder, group1, group2):
+    """
+    OBSOLETE: using 3dttest++ also calculates the mean maps for each group as well as difference map.
+    created with the WET (write everything twice) principle :(
+    """
+    outfile1 = os.path.join(out_folder,f'mean{map_name}_group1.nii')
+    filelist_formatted = " ".join(map(str, group1))
+    runAFNI(f"3dMean -prefix {outfile1} {filelist_formatted}", printout=False)
+    print(f"created {outfile1}")
+    
+    outfile2 = os.path.join(out_folder,f'mean{map_name}_group2.nii')
+    filelist_formatted = " ".join(map(str, group2))
+    runAFNI(f"3dMean -prefix {outfile2} {filelist_formatted}", printout=False)
+    print(f"created {outfile2}")
+    
+    # also create the difference map:
+    outfile3 = os.path.join(out_folder,f'mean{map_name}_group1_MINUS_group2.nii')
+    runAFNI(f"3dcalc -prefix {outfile3} -a {outfile1} -b {outfile2} -expr 'a-b'", printout=False)
+    print(f"created {outfile3}")
+
+
+def ttest_2groups(outfolder, map_name, group1, group2, blur=0.0):
+    outfile = os.path.join(outfolder,f'mean{map_name}_Ttest.nii')
+    filelist_formatted1 = " ".join(map(str, group1))
+    filelist_formatted2 = " ".join(map(str, group2))
+    runAFNI(f"3dttest++ -prefix {outfile} -setA {filelist_formatted1} -labelA group1 -setB {filelist_formatted2} -labelB group2 -toz  -exblur {blur} -overwrite", printout=False)
+    print(f"created {outfile}")
+    
+    
+def custom_detrend(x, baseline):
+    """
+    For very common problem:
+    Linear (or polynomial) detrending of time-series x.
+    Fit is only performed for the initial baseline period (units for bsl are just array points, not seconds!)
+    but extrapolated fit curve is subtracted over whole time series x (mean is re-added).
+    Afni tools etc only perform fits on whole time series.
+    """ 
+    t = np.arange(0,x.size)
+    t_bsl = np.arange(0,baseline)
+    fit = np.polyfit(t_bsl, x[0:baseline], 1)
+    trendline = np.polyval(fit, t)
+    x_detrended = x - trendline + np.mean(x)
+    return x_detrended, trendline
